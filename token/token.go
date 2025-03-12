d8ab71f76aa6b036dfb9b7e4f1ae5e4aa99f3019
shadow-dom <jacob.r.holcomb+1@gmail.com>
2025-03-11 21:15:10 -0400

    Initial commit.

---
+++ token/token.go
@@ -0,0 +1,18 @@
+package token
+
+const (
+	ILLEGAL   = "ILLEGAL"
+	EOF       = "EOF"
+	IDENT     = "IDENT"
+	INT       = "INT"
+	ASSIGN    = "="
+	PLUS      = "+"
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	FUNCTION  = "FUNCTION"
+	LET       = "LET"
+)
